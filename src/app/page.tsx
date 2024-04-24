2a045a170f9f5b375595ba094b8d5ae966c1f73a
Kennedy Martins <kennedysmartins@hotmail.com>
2024-04-23 23:01:50 -0300

    fix issues small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
   return (
     <>
       <Navigation />
-      <main className=" container text-2xl gap-2 my-10">
+      <main className=" container text-2xl gap-2 my-5">
         <div className="flex items-center justify-center">
           <Image
             src="/gameplan-logo.png"
-            width={200}
-            height={200}
+            width={170}
+            height={170}
             alt="GamePlan Logo"
           />
         </div>
