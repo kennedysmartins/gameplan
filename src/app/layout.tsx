bf4efda382d1a18e5232ae3a68666f0bfc166fba
Kennedy Martins <kennedysmartins@hotmail.com>
2024-04-24 17:57:34 -0300

    remove navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
           defaultTheme="dark"
           disableTransitionOnChange
         >
-      <Navigation />
+      {/* <Navigation /> */}
 
           {children}
         </ThemeProvider>
