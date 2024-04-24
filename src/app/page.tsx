0e99c9422ab1138418b6bc5d0e723f13da5bb0c9
Kennedy Martins <kennedysmartins@hotmail.com>
2024-04-23 22:38:07 -0300

    new logo and main screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-import Navbar from "@/components/Navbar"
-import Navigation from "@/components/Navigation"
+import Navbar from "@/components/Navbar";
+import Navigation from "@/components/Navigation";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Navigation />
       <main className=" container text-2xl gap-2 my-10">
-        <h1>Hello</h1>
-        
+        <div className="flex items-center justify-center">
+          <Image
+            src="/gameplan-logo.png"
+            width={200}
+            height={200}
+            alt="GamePlan Logo"
+          />
+        </div>
+        <div className="flex flex-col items-center gap-4">
+          <Button variant={"default"}>Nova Partida</Button>
+          <Button variant={"outline"}>Configurações</Button>
+        </div>
       </main>
     </>
-  )
+  );
 }
