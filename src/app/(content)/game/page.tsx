857e01bb989d2cb50b6459eb13c3e27415ce3a6c
Kennedy Martins <kennedysmartins@hotmail.com>
2024-04-26 19:24:33 -0300

    added route to T2R

--- src/app/(content)/game/page.tsx
+++ src/app/(content)/game/page.tsx
@@ -6,25 +6,44 @@ const game = () => {
   return (
     <main className=" container text-2xl gap-2 my-5">
       <div className="flex items-center justify-center my-8">
-          <img
-            src="/select-game.png"
-            alt="GamePlan Logo"
-            className="w-56"
-          />
-        </div>
+        <img src="/select-game.png" alt="GamePlan Logo" className="w-56" />
+      </div>
       <div className="flex flex-col items-center gap-4">
         <div className="flex flex-wrap gap-4 w-56 items-center justify-center">
           <Link href="/game/7wonders">
-
-          <Button className="w-24" variant={"outline"}><img src="https://cdn.svc.asmodee.net/production-unboxnowcom/uploads/2021/12/7Wonders-noirblanc.png" alt="7 Wonders"/></Button>
+            <Button className="w-24" variant={"outline"}>
+              <img
+                src="https://cdn.svc.asmodee.net/production-unboxnowcom/uploads/2021/12/7Wonders-noirblanc.png"
+                alt="7 Wonders"
+              />
+            </Button>
           </Link>
-          <Button className="w-24" variant={"outline"}><img src="https://cdn.svc.asmodee.net/production-unboxnowcom/uploads/2022/01/TTR_LogoWhite-1.png" alt="7 Wonders"/></Button>
-          <Button className="w-24" variant={"default"}>GWT</Button>
-          <Button className="w-24" variant={"default"}>S.M</Button>
-          <Button className="w-24" variant={"default"}>Bonfire</Button>
-          <Button className="w-24" variant={"default"}>Castle</Button>
-          <Button className="w-24" variant={"default"}>Sagrada</Button>
-          <Button className="w-24" variant={"default"}>Outros</Button>
+          <Link href="/game/T2R">
+            <Button className="w-24" variant={"outline"}>
+              <img
+                src="https://cdn.svc.asmodee.net/production-unboxnowcom/uploads/2022/01/TTR_LogoWhite-1.png"
+                alt="7 Wonders"
+              />
+            </Button>
+          </Link>
+          <Button className="w-24" variant={"default"}>
+            GWT
+          </Button>
+          <Button className="w-24" variant={"default"}>
+            S.M
+          </Button>
+          <Button className="w-24" variant={"default"}>
+            Bonfire
+          </Button>
+          <Button className="w-24" variant={"default"}>
+            Castle
+          </Button>
+          <Button className="w-24" variant={"default"}>
+            Sagrada
+          </Button>
+          <Button className="w-24" variant={"default"}>
+            Outros
+          </Button>
         </div>
       </div>
     </main>
